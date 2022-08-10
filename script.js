c155013ef99329e8100a2fc758e951ef2c24556f
Noah Segal <n.h.segal@gmail.com>
2022-08-10 16:26:13 +0900

    Create a pop up window for about

---
+++ script.js
@@ -0,0 +1,24 @@
+let aboutbuggy = document.getElementById("aboutbuggy");
+
+
+function showNotes(section) {
+    section.style.display = "block";
+}
+
+notesClose.onclick = function () {
+    notesModal.style.display = "none";
+}
+
+function showNotes(){
+    let 
+}
+switch(expression) {
+    case x:
+      // code block
+      break;
+    case y:
+      // code block
+      break;
+    default:
+      // code block
+  }
